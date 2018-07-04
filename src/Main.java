96073b15efc0280127da254e444f4d7cc24d416b
Magda Kubin <m.mielnicka79@gmail.com>
2018-07-04 18:57:34 +0200

    VOL. 4

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Uczę\nsię\nprogramowania");
         System.out.println("i\ncommitowania\n");
-        System.out.println("Przechodzimy do kolejnej wersji programu.\nTo trzecia opcja.");
+
+        System.out.println("Czwarta opcja.\nZauważyłam,że na githubie nie ma poprzednich wersji.");
     }
 }
