4b5e9f86e8a391aa69653544a0988953699f3a68
Magda Kubin <m.mielnicka79@gmail.com>
2018-07-04 18:52:27 +0200

    VOL. 3

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Uczę\nsię\nprogramowania");
         System.out.println("i\ncommitowania\n");
+        System.out.println("Przechodzimy do kolejnej wersji programu.\nTo trzecia opcja.");
     }
 }
